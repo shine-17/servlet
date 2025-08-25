c52fe19324e65efe875935eb18888d1c95157256
dohun <shine_17@naver.com>
2025-08-25 21:39:35 +0900

    add 6 section: ViewResolver, @RequestMapping을 통한 Spring MVC 패턴 적용

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -2,18 +2,16 @@ package hello.servlet.web.springmvc.old;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-//@Qualifier("/springmvc/old-controller")
 @Component("/springmvc/old-controller")
 public class OldController implements Controller {
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form");
     }
 }
